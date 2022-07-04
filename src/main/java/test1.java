f29394c96341c74f9fee2061e3c553bbf754212e
huchao <1310259975@qq.com>
2022-07-04 14:07:36 +0800

    Initial commit

---
+++ src/main/java/test1.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : huc
+ * @Project : FlinksqlTest
+ * @Package : PACKAGE_NAME
+ * @createTime : 2022/7/4 13:45
+ * @Email : 1310259975@qq.com
+ * @Description :
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
